1ec22ce3c843fbbd83ddcff5749468849c4fbfc3
Daniel Posch <daniel.posch@aau.at>
2015-02-10 15:06:39 +0100

    added mratio.

---
+++ extensions/fw/mratio.cc
@@ -0,0 +1,25 @@
+#include "mratio.h"
+
+using namespace nfd;
+using namespace nfd::fw;
+
+Mratio::Mratio(std::vector<int> faces) : SAFStatisticMeasure(faces)
+{
+}
+
+void Mratio::logSatisfiedInterest(shared_ptr<pit::Entry> pitEntry,const Face& inFace, const Data& data)
+{
+  int ilayer = SAFStatisticMeasure::determineContentLayer(pitEntry->getInterest());
+  stats[ilayer].statisfied_requests[inFace.getId ()] += 1;
+}
+
+void Mratio::logExpiredInterest(shared_ptr<pit::Entry> pitEntry)
+{
+  int ilayer = SAFStatisticMeasure::determineContentLayer(pitEntry->getInterest());
+
+  const nfd::pit::OutRecordCollection records = pitEntry->getOutRecords();
+  for(nfd::pit::OutRecordCollection::const_iterator it = records.begin (); it!=records.end (); ++it)
+  {
+    stats[ilayer].unstatisfied_requests[(*it).getFace()->getId()] += 1;
+  }
+}
---
+++ extensions/fw/mratio.h
@@ -0,0 +1,26 @@
+#ifndef MRATIO_H
+#define MRATIO_H
+
+#include "safstatisticmeasure.h"
+#include <iostream>
+
+namespace nfd
+{
+namespace fw
+{
+class Mratio : public SAFStatisticMeasure
+{
+public:
+  Mratio(std::vector<int> faces);
+
+  virtual void logSatisfiedInterest(shared_ptr<pit::Entry> pitEntry,const Face& inFace, const Data& data);
+  virtual void logExpiredInterest(shared_ptr<pit::Entry> pitEntry);
+
+protected:
+
+
+};
+
+}
+}
+#endif // MRATIO_H
