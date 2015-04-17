41291e18e8c13f3b6f9a5dd47f6819ee712518f9
dazedsheep <benjamin.rainer@itec.aau.at>
2015-04-17 15:08:45 +0200

    Finished implementations for JSAC Journal

--- matlab/LP/jsac.m
+++ matlab/LP/jsac.m
@@ -23,55 +23,87 @@ cl = parser.clients;
 
 [optim_target, solution_clients, solution_graph, avg_bitrate, exitflag] =  solveFractionalMultiCommodityFlow(g, cl);
 
-
 %% now solve the same scenario but with caching
 % therefore, we have to check all combinations of clients that try to
 % stream the same content
 % BEWARE: there may be many combinations that this script will check until
 % it finds the optimal one!!
+% THIS MAY TAKE SOME TIME...
 
 combs = combvec(parser.servers_clients{:});
 fprintf('Going to check %d combinations for an optimal solution for the caching scenario!\n', size(combs,2));
 
 maxV = 0;
 iteration = 1;
-%for c = 1:size(combs, 2)
-c = 1;
-fprintf('Computing combination %d...\n', c);
-tmp_cl  = cell(size(combs, 1),1);
-for i=1:size(combs, 1)
-    tmp_cl{i} = copy(cl{combs(i,c)});
-end
-[optim_target_caching, solution_clients_caching, solution_graph_caching, avg_bitrate_caching, exitflag] =  solveFractionalMultiCommodityFlow(g, tmp_cl);
-if optim_target_caching > maxV
-    maxV = optim_target_caching;
-    iteration = c;
-end
-fprintf('Critaria for optimality: %f, for iteration %d.\n', optim_target_caching, c);
 
-fprintf('Going to calculate optimal cache hits bitrates for the other %d, for iteration %d.\n', length(cl) - length(tmp_cl), c);
-
-% generate the list of new server nodes for the clients of each content
-% the new server nodes are nodes that are on the paths for the clients
-% that "pre"-fetched the same content ...
-% due to the machine precision we only take paths into accoutn which
-% have a bitrate > 1
-%%
-new_servers = cell(length(tmp_cl),1);
-for i = 1:size(combs,1)
-    for j = 1:length(tmp_cl{i}.paths)
+for c = 1:size(combs, 2)
+    fprintf('Computing combination %d...\n', c);
+    tmp_cl  = cell(size(combs, 1),1);
+    
+    for i=1:size(combs, 1)
+        tmp_cl{i} = copy(cl{combs(i,c)});
+    end
+    [optim_target_caching, solution_clients_caching, solution_graph_caching, avg_bitrate_caching, exitflag] =  solveFractionalMultiCommodityFlow(g, tmp_cl);
+    
+    % TODO: think of moving the calc for all other clients into every loop
+    % and devide on an average bitrate overall clients... RETHINK THIS!
+    
+    if optim_target_caching > maxV
+        maxV = optim_target_caching;
+        iteration = c;
         
-        if tmp_cl{i}.paths{j}.bitrate > 1
-            tmp_cl{i}.paths{j}.printMe();
-            for k = 2:length(tmp_cl{i}.paths{j}.myPath)-1;
-                new_servers{i} = [new_servers{i}, tmp_cl{i}.paths{j}.myPath(k)];
+        % if there is a better caching solution possible empty the cell
+        % arrays to save memory:
+        % IF YOU HAVE ENOUGH MEMORY JUST COMMENT THE FOLLOWING LINES OF CODE!
+        optim_target_caching_full_proc = cell(size(combs,2),1);
+        solution_clients_caching_full_proc = cell(size(combs,2),1);
+        solution_graph_caching_full_proc = cell(size(combs,2),1);
+        avg_bitrate_caching_full_proc = cell(size(combs,2),1);
+        exitflag_full_proc = cell(size(combs,2),1);
+        % END OF POSSIBLE DELETION
+        
+        fprintf('Going to calculate optimal cache hit bitrates for the other %d clients, for iteration %d.\n', length(cl) - length(tmp_cl), c);
+        
+        % generate the list of new server nodes for the clients of each content
+        % the new server nodes are nodes that are on the paths for the clients
+        % that "pre"-fetched the same content ...
+        % due to the machine precision we only take paths into accoutn which
+        % have a bitrate > 1
+        %%
+        tmp_cl_caching = cell(length(cl) - length(parser.servers_clients), 1);
+        new_clients = 0;
+        new_servers = cell(length(tmp_cl),1);
+        for i = 1:size(combs,1)
+            for j = 1:length(tmp_cl{i}.paths)
+                
+                if tmp_cl{i}.paths{j}.bitrate > 1
+                    %tmp_cl{i}.paths{j}.printMe();
+                    for k = 2:length(tmp_cl{i}.paths{j}.myPath)-1;
+                        new_servers{i} = [new_servers{i}, tmp_cl{i}.paths{j}.myPath(k)];
+                    end
+                end
+                
+            end
+            
+            new_servers{i} = unique(new_servers{i});
+            combos = parser.servers_clients{i};
+            for k=1:length(combos)
+                if tmp_cl{i}.id ~=  cl{combos(k)}.id
+                    new_clients = new_clients + 1;
+                    tmp_cl_caching{new_clients} = copy(cl{combos(k)});
+                    tmp_cl_caching{new_clients}.maxBitrate = tmp_cl{i}.maxPossibleBitrateForStreaming;
+                    tmp_cl_caching{new_clients}.paths = solution_graph_caching.calcPaths(tmp_cl_caching{new_clients}.start_vertex, new_servers{i});
+                    fprintf('Found %3d paths for client %d to the nodes that cached the appropriate content.\n', length(tmp_cl_caching{new_clients}.paths), tmp_cl_caching{new_clients}.id);
+                end
             end
         end
+        % now solve the new problem
+        [optim_target_caching_full_proc{c}, solution_clients_caching_full_proc{c}, solution_graph_caching_full_proc{c}, avg_bitrate_caching_full_proc{c}, exitflag_full_proc{c}] =  solveFractionalMultiCommodityFlow(solution_graph_caching, tmp_cl_caching);
         
     end
-    new_servers{i} = unique(new_servers{i});
+    fprintf('Critaria for optimality: %f, for iteration %d.\n', optim_target_caching, c);
 end
-%end
+
 %%
 % finally solve the problem once more for the optimal combination
 tmp_cl = cell(size(combs, 1),1);
