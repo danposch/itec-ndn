ca521a80ab34ecc6bdd4404d6043afdcc4641b3e
Daniel Posch <daniel.posch@aau.at>
2015-04-22 10:22:28 +0200

    added python script for jsac simulations.

---
+++ python_scripts/dashplayer_stats.py
@@ -0,0 +1,143 @@
+#! /usr/bin/python
+
+import time
+import numpy
+import os
+import glob
+import sys
+import collections
+import re
+       
+
+###functions####################################################################
+
+TIME_INDEX = 0
+NODE_INDEX = 1
+SEGMENT_NR_INDEX = 2
+SEGMENT_DUR_INDEX = 3
+SEGMENT_REP_INDEX = 4
+SEGMENT_BITRATE_INDEX = 5
+STALLING_MSEC_INDEX = 6
+
+def process_dash_trace(f):
+	
+
+	file = open(f, "r")
+	next(file) #skip header
+
+	stats = {SEGMENT_REP_INDEX: 0.0,
+					STALLING_MSEC_INDEX: 0.0,
+					SEGMENT_BITRATE_INDEX: 0.0}
+	
+	line_counter = 0
+	for line in file:
+		l = re.split(r'\t+',line.rstrip('\t'))		
+		#print l
+		if(len(l) < STALLING_MSEC_INDEX+1):
+			continue
+
+		#print "adding stats"
+		stats[SEGMENT_REP_INDEX] += float(l[SEGMENT_REP_INDEX])
+		stats[SEGMENT_BITRATE_INDEX] += float(l[SEGMENT_BITRATE_INDEX])
+		stats[STALLING_MSEC_INDEX] += float(l[STALLING_MSEC_INDEX])
+		line_counter += 1
+
+	if(line_counter > 0):
+		stats[SEGMENT_REP_INDEX] /= line_counter
+		stats[SEGMENT_BITRATE_INDEX] /= line_counter
+
+	#print stats
+	return stats
+
+def process_cs_trace(file):
+	#print("process_cs_trace(str(file))
+
+	f = open(file,"r");
+
+	stats = {}
+
+	TIME_INDEX = 0
+	NODE_INDEX = 1
+	TYPE_INDEX = 2
+	PACKETS_COUNT_INDEX = 3
+
+	#create first lvl of dictionary and init second level with 0
+	for line in f:
+		l = line.split('\t')
+		if(len(l) < PACKETS_COUNT_INDEX+1):
+			continue
+
+		if("CacheHits" in l[TYPE_INDEX] or "CacheMisses" in l[TYPE_INDEX]):
+			if(l[NODE_INDEX] not in stats):
+				stats[l[NODE_INDEX]] = {}
+				stats[l[NODE_INDEX]].update({'CacheHits': 0})
+				stats[l[NODE_INDEX]].update({'CacheMisses': 0})
+			
+			if("CacheHits" in l[TYPE_INDEX]):
+				stats[l[NODE_INDEX]]['CacheHits'] += int(l[PACKETS_COUNT_INDEX])
+			
+			if("CacheMisses" in l[TYPE_INDEX]):
+				stats[l[NODE_INDEX]]['CacheMisses'] += int(l[PACKETS_COUNT_INDEX])
+		
+	return stats
+
+###programm#####################################################################
+
+def computeStats(curdir):
+
+	print "Curring working dir = " + curdir + "\n"
+
+	#process dashplayer-trace_*
+	files = glob.glob(curdir + "/dashplayer-trace_*.txt" );
+	output_file = open(curdir+"/DASH-STATS.txt", "w")
+
+	###statistic over all clients
+
+	output_file.write("Statistic over all Clients:\n")
+
+	avg_level_per_client = 0.0
+	avg_stalls_msec_per_client = 0.0
+	avg_consumed_video_bitrate = 0.0
+
+	for f in  files:
+		
+		#print f
+		stats = process_dash_trace(f)
+		
+		avg_level_per_client += stats[SEGMENT_REP_INDEX]
+		avg_stalls_msec_per_client += stats[STALLING_MSEC_INDEX]
+		avg_consumed_video_bitrate += stats[SEGMENT_BITRATE_INDEX]
+		
+	avg_level_per_client /= len(files)
+	avg_stalls_msec_per_client /= len(files) 
+	avg_consumed_video_bitrate /= len(files)
+
+	output_file.write("AVG Level per Client: " + str(avg_level_per_client) + "\n")
+	output_file.write("AVG Stalls (msec): " + str(avg_stalls_msec_per_client) + "\n")
+	output_file.write("AVG Video Bitrate (bit/s):" + str(avg_consumed_video_bitrate) + "\n")
+
+	#well here we can process cs-trace.txt, if we want:
+
+	run_cache_hits = 0.0
+	run_cache_misses = 0.0
+	run_cache_hit_ratio = 0.0
+
+	if os.path.exists(curdir+"/cs-trace.txt"):
+		cs_stats = process_cs_trace(curdir+"/cs-trace.txt")
+		for key in cs_stats:
+			run_cache_hits += cs_stats[key]['CacheHits']
+			run_cache_misses += cs_stats[key]['CacheMisses']
+	else:
+		print "Could not find cs-trace.txt, skipped parsing cache-hitratio"
+
+	run_cache_hit_ratio = run_cache_hits / (run_cache_hits+run_cache_misses)
+	output_file.write( "Cache_Hit_Ratio:" + str(run_cache_hit_ratio)+"\n")
+
+# check if script has been called with a parameter
+for arg in sys.argv:
+	if 'dashplayer_stats.py' in arg:
+		print "Calculating Average via commandline for folder output/, writeing results to output/STATS.txt:"
+		computeStats("output/")
+		
+# end for
+
---
+++ python_scripts/fixed_dash_simulation.py
@@ -0,0 +1,350 @@
+#! /usr/bin/python
+
+import time
+import numpy
+import os
+import glob
+import collections
+import shutil
+import re
+import subprocess
+from subprocess import call
+import threading
+import time
+import operator
+import random
+import dashplayer_stats as dashplayer_stats
+import consumer_stats as consumer_stats
+
+curActiveThreads = 0
+invalid_runs = 0
+
+class Thread(threading.Thread):
+    # init
+  def __init__(self,job_number, sys_cal, callback_method, src ,dst):
+		super(Thread,self).__init__()
+		self.sysCall = sys_cal
+		self.jobNumber = job_number
+		self.callback = callback_method
+		self.src = src
+		self.dst = dst
+
+  # overwriting run method of threading.Thread (do not call this method, call thread.start() )
+  def run(self):
+
+		if not os.path.exists(self.src+"/traces"):
+			os.makedirs(self.src+"/traces")
+
+		fpOut = open("t_" + str(self.jobNumber) + ".stdout.txt", "w")
+
+		# start subprocess
+		proc = subprocess.Popen(self.sysCall,stdout=fpOut, cwd=self.src)
+		proc.communicate() # wait until finished
+
+		# sleep 0.5 seconds to be sure the OS really has finished the process
+		time.sleep(0.5)
+
+		fpOut.close()
+		os.remove("t_" + str(self.jobNumber) + ".stdout.txt")
+
+		# callback
+		print "threadFinished(job_" + str(self.jobNumber) + ")"
+		self.callback(self.jobNumber,self.src,self.dst, proc.returncode)
+
+def threadFinished(job_number,src,dst,returncode):
+	#compute statistics
+
+	global curActiveThreads, invalid_runs
+
+	if(returncode != 0):
+		invalid_runs += 1
+		print "Error in job_" + str(job_number) +". Simulation incomplete!"
+	else:
+		print "computeStats(job_" + str(job_number) + ")"
+		try:
+			print src
+			dashplayer_stats.computeStats(src+"/traces/")
+			#consumer_stats.generateStatsPerSimulation(src)
+		except Exception:
+			invalid_runs += 1
+			pass
+
+	#copy results
+	files = glob.glob(src + "/traces/*STATS*.txt")
+	files.extend(glob.glob(src + "/traces/*dashplayer-trace*.txt"))
+	files.extend(glob.glob(src + "/traces/*cs-trace*.txt"))
+	#files = glob.glob(src + "/traces/*.txt") # copy everthing
+
+	if not os.path.exists(dst):
+		os.makedirs(dst)
+
+	for f in files:
+		shutil.move(f, dst+"/"+os.path.basename(f))
+
+	#print "DELTE FOLDER " + src
+	shutil.rmtree(src)
+
+	print "statsCollected(job_" + str(job_number) + ")"
+
+	curActiveThreads -= 1
+		
+def getScenarioName(strategy,linkfailure,adap):
+	
+	name = ""
+
+	if("fw-strategy=bestRoute" in strategy):
+		name += "BestRoute"
+	elif("fw-strategy=smartflooding" in strategy):
+		name += "SmartFlooding"
+	elif("fw-strategy=saf" in strategy):
+		name += "SAF"
+	elif("fw-strategy=broadcast" in strategy):
+		name += "Broadcast"
+	elif("fw-strategy=ncc" in strategy):
+		name += "NCC"
+	else:
+		name += "UnknownStrategy"
+
+	if("route=single" in strategy):
+		name += "_SingleRoute"
+	elif("route=all" in strategy):
+		name += "_AllRoutes"
+	else:
+		name += "_UnkownRoute"
+
+	if("--adaptation=buffer" in adap):
+		name += "_BufferAdap"
+	elif("--adaptation=rate" in adap):
+		name += "_RateAdap"
+	elif("--adaptation=nologic" in adap):
+		name += "_NoAdap"
+	else:
+		name += "_UnknownAdaptation"
+
+	failure = re.findall('\d+', linkfailure)
+
+	if(len(failure) == 1):
+		name += "_LinkFailure_"+failure[0]
+	else:
+		name +="_UnkownLinkFailurey"
+
+	return name
+
+def	order_dash_results(path):
+	results_level = {}
+	results_stalls = {}
+	results_bitrate = {}
+	results_cachehits = {}
+
+	for root, dirs, files in os.walk(path):
+		for subdir in dirs:
+		
+			if "output_run" in subdir:
+				continue
+
+			#print root+subdir
+
+			files = glob.glob(root+subdir + "/*/DASH-STATS.txt" ) #match DASH-STATS exectly as there is STATS too
+		
+			avg_layer = 0.0
+			avg_stalls_msec = 0.0
+			avg_video_bitrate = 0.0
+			avg_cache_hitratio = 0.0
+			file_count = 0
+
+			for file in files:
+
+				#print file
+				f = open(file, "r")
+				for line in f:
+					if(line.startswith("AVG Level per Client:")):
+						avg_layer += float(line[len("AVG Level per Client:"):])
+						
+					if(line.startswith("AVG Stalls (msec):")):
+						avg_stalls_msec += float(line[len("AVG Stalls (msec):"):])
+
+					if(line.startswith("AVG Video Bitrate (bit/s):")):
+						avg_video_bitrate += float(line[len("AVG Video Bitrate (bit/s):"):])
+
+					if(line.startswith("Cache_Hit_Ratio:")):
+						avg_cache_hitratio += float(line[len("Cache_Hit_Ratio:"):])
+					
+				file_count +=1
+			
+
+			if(file_count > 0):
+	 			avg_layer /= file_count
+				avg_stalls_msec /= file_count
+				avg_video_bitrate /= file_count
+				avg_cache_hitratio /= file_count
+	
+			#print avg_ratio
+			results_level.update({"AVG Level per Client:"+ subdir : avg_layer})
+			results_stalls.update({"AVG Stalls (msec):"+ subdir : avg_stalls_msec})
+			results_bitrate.update({"AVG Video Bitrate (bit/s):"+ subdir : avg_video_bitrate})
+			results_cachehits.update({"Cache_Hit_Ratio:"+ subdir : avg_cache_hitratio})
+
+	result_list = [results_level, results_stalls, results_bitrate, results_cachehits]
+
+	f = open(path + "/dash_result.txt", "w")
+	for results in result_list: 
+		sorted_results = reversed(sorted(results.items(), key=operator.itemgetter(1)))
+		for entry in sorted_results:
+			f.write(entry[0] + ": " + str(entry[1]) + "\n")
+		f.write("\n\n")
+
+'''def	order_consumer_results(path):
+	results = {}
+
+	for root, dirs, files in os.walk(path):
+		for subdir in dirs:
+		
+			if "output_run" in subdir:
+				continue
+
+			#print root+subdir
+
+			files = glob.glob(root+subdir + "/*/STATS.txt" ) # match stats exectly as there is DASH-STATS too
+		
+			avg_ratio = 0.0
+			file_count = 0
+			cache_hit_ratio = 0.0
+
+			for file in files:
+
+				#print file
+				f = open(file, "r")
+				for line in f:
+					if(line.startswith("Ratio:")):
+						avg_ratio += float(line[len("Ratio:"):])
+						
+					if(line.startswith("Cache_Hit_Ratio:")):
+						cache_hit_ratio += float(line[len("Cache_Hit_Ratio:"):])
+					
+				file_count +=1
+			
+
+			if(file_count > 0):
+	 			avg_ratio /= file_count
+				cache_hit_ratio /= file_count
+	
+			#print avg_ratio
+			results.update({"AVG_RATIO:"+ subdir : avg_ratio})
+			results.update({"CACHE_HIT_RATIO:"+ subdir : cache_hit_ratio})
+
+	sorted_results = reversed(sorted(results.items(), key=operator.itemgetter(1)))
+	f = open(path + "/result.txt", "w")
+	for entry in sorted_results:
+		f.write(entry[0] + ":" + str(entry[1]) + "\n")
+'''	
+
+###NOTE Start this script FROM itec-ndn MAIN-FOLDER!!!
+
+SIMULATION_DIR=os.getcwd()
+
+THREADS = 1
+SIMULATION_RUNS = 1
+SIMULATION_OUTPUT = SIMULATION_DIR + "/output/"
+
+#order_dash_results(SIMULATION_OUTPUT)
+#exit(0)
+
+#brite config file
+scenario="dash_fixed_top"
+britePath="/home/dposch/ndnSIM/itec-ndn/"
+
+briteConfig="--briteConfFile="+britePath+"brite_configs/fixed.conf"
+
+singleRoute="--route=single"
+allRoute="--route=all"
+
+bestRoute="--fw-strategy=bestRoute " + allRoute
+ncc="--fw-strategy=ncc " + allRoute
+broadcast="--fw-strategy=broadcast " + allRoute
+saf="--fw-strategy=saf " + allRoute
+
+forwardingStrategies = [bestRoute, broadcast, saf, ncc]
+#forwardingStrategies = [saf,bestRoute]
+
+buf = "--adaptation=buffer"
+rate = "--adaptation=rate"
+no = "--adaptation=nologic"
+
+adaptationStrategies = [buf, rate, no]
+
+#linkFailures = ["--linkFailures=0", "--linkFailures=15", "--linkFailures=30", "--linkFailures=50", "--linkFailures=100"]
+#linkFailures = ["--linkFailures=30", "--linkFailures=50", "--linkFailures=100"]
+linkFailures = ["--linkFailures=0"]
+
+SCENARIOS = {}
+
+settings_counter = 0
+for strategy in forwardingStrategies:
+	for adap in adaptationStrategies:
+		for failures in linkFailures:
+			name = getScenarioName(strategy,failures,adap) 
+			SCENARIOS.update({name : { "executeable": scenario, "numRuns": SIMULATION_RUNS, "params": [strategy, adap, failures, briteConfig] }})			
+		settings_counter += 1
+
+#build project before
+call([SIMULATION_DIR + "/waf"])
+
+###script start
+print "\nCurring working dir = " + SIMULATION_DIR + "\n"
+
+print str(settings_counter) + " different settings selected"
+print str(SIMULATION_RUNS) + " runs per setting"
+print "Total runs: " + str(settings_counter*SIMULATION_RUNS)
+
+time.sleep(5)
+
+###script start
+print "\nCurring working dir = " + SIMULATION_DIR + "\n"
+
+job_number = 0
+
+for scenarioName in SCENARIOS.keys():
+	runs = SCENARIOS[scenarioName]['numRuns']
+	executeable = SCENARIOS[scenarioName]['executeable']
+	
+	executeable = "build/" + executeable
+	print "------------------------------------------------------------------------"
+	print "Starting", runs , "simulations of", scenarioName
+
+  #REMOVE JUST FOR SMALL TEST
+  #if(job_number>4):
+    #continue
+	
+	for i in range(0, runs):
+  	# See if we are using all available threads
+		while curActiveThreads >= THREADS:
+			time.sleep(1) # wait 1 second
+
+		print "----------"
+		print "Simulation run " + str(i) + " in progress..." 
+		sysCall = [SIMULATION_DIR+"/" + executeable] +  SCENARIOS[scenarioName]['params'] + ["--RngRun=" + str(1+i)] + ["--outputFolder=traces"] ## working folder of subprocess is determined by Thread
+		print sysCall
+
+		dst = SIMULATION_OUTPUT+scenarioName + "/output_run"+str(i)
+		src = SIMULATION_OUTPUT+"../ramdisk/tmp_folder_" + str(job_number)
+
+	   # start thread, get callback method to be called when thread is done
+		thread = Thread(job_number, sysCall, threadFinished, src, dst)
+		#if(job_number != 0 and job_number<THREADS): # do this to avoid that all threads copy at the same time
+			#time.sleep(15.0)
+		thread.start()
+
+		job_number += 1
+		curActiveThreads += 1	
+# end for
+
+while curActiveThreads != 0:
+    time.sleep(15)
+    print "Active Threads: " + str(curActiveThreads)
+
+order_dash_results(SIMULATION_OUTPUT) #dash_results
+#order_consumer_results(SIMULATION_OUTPUT) # basic consumer results
+
+print ""
+print "We had " + str(invalid_runs) + " invalid runs"
+print "Finished."
