2f4a0cb3ed1644a0f53d6d158d9198a84711bd8f
Daniel Posch <daniel.posch@aau.at>
2015-07-28 08:25:58 +0200

    .

--- python_scripts/dashplayer_stats.py
+++ python_scripts/dashplayer_stats.py
@@ -19,6 +19,8 @@ SEGMENT_REP_INDEX = 4
 SEGMENT_BITRATE_INDEX = 5
 STALLING_MSEC_INDEX = 6
 
+ALOGIC_NUMBER_SWITCHES_INDEX = 99
+
 def process_dash_trace(f):
 	
 
@@ -27,9 +29,11 @@ def process_dash_trace(f):
 
 	stats = {SEGMENT_REP_INDEX: 0.0,
 					STALLING_MSEC_INDEX: 0.0,
-					SEGMENT_BITRATE_INDEX: 0.0}
+					SEGMENT_BITRATE_INDEX: 0.0,
+					ALOGIC_NUMBER_SWITCHES_INDEX: 0.0}
 	
 	line_counter = 0
+	last_rep_layer = -1
 	for line in file:
 		l = re.split(r'\t+',line.rstrip('\t'))		
 		#print l
@@ -40,6 +44,14 @@ def process_dash_trace(f):
 		stats[SEGMENT_REP_INDEX] += float(l[SEGMENT_REP_INDEX])
 		stats[SEGMENT_BITRATE_INDEX] += float(l[SEGMENT_BITRATE_INDEX])
 		stats[STALLING_MSEC_INDEX] += float(l[STALLING_MSEC_INDEX])
+
+		if line_counter > 1:
+			if(last_rep_layer != l[SEGMENT_REP_INDEX]):
+				stats[ALOGIC_NUMBER_SWITCHES_INDEX] += 1
+				last_rep_layer = l[SEGMENT_REP_INDEX]
+		else:
+			last_rep_layer = l[SEGMENT_REP_INDEX]
+
 		line_counter += 1
 
 	if(line_counter > 0):
@@ -98,6 +110,7 @@ def computeStats(curdir):
 	avg_level_per_client = 0.0
 	avg_stalls_msec_per_client = 0.0
 	avg_consumed_video_bitrate = 0.0
+	avg_quality_switches = 0.0
 
 	for f in  files:
 		
@@ -107,14 +120,17 @@ def computeStats(curdir):
 		avg_level_per_client += stats[SEGMENT_REP_INDEX]
 		avg_stalls_msec_per_client += stats[STALLING_MSEC_INDEX]
 		avg_consumed_video_bitrate += stats[SEGMENT_BITRATE_INDEX]
+		avg_quality_switches += stats[ALOGIC_NUMBER_SWITCHES_INDEX]
 		
 	avg_level_per_client /= len(files)
 	avg_stalls_msec_per_client /= len(files) 
 	avg_consumed_video_bitrate /= len(files)
+	avg_quality_switches /= len(files)
 
 	output_file.write("AVG Level per Client: " + str(avg_level_per_client) + "\n")
 	output_file.write("AVG Stalls (msec): " + str(avg_stalls_msec_per_client) + "\n")
 	output_file.write("AVG Video Bitrate (bit/s):" + str(avg_consumed_video_bitrate) + "\n")
+	output_file.write("AVG Number of Switches):" + str(avg_quality_switches) + "\n")
 
 	#well here we can process cs-trace.txt, if we want:
 
