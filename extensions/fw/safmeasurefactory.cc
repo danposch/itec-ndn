4c8f50b97316ab3bebda5f84896ef32293cd0270
Daniel Posch <daniel.posch@aau.at>
2016-02-18 16:00:31 +0100

    added hop-based measure.

--- extensions/fw/safmeasurefactory.cc
+++ extensions/fw/safmeasurefactory.cc
@@ -43,13 +43,14 @@ boost::shared_ptr<SAFStatisticMeasure> SAFMeasureFactory::getMeasure(std::string
 
   if(mmap.end () != match)
   {
-    fprintf(stderr, "Found match for %s: %s\n", name.c_str (), match->first.c_str());
+    //fprintf(stderr, "Found match for %s: %s\n", name.c_str (), match->first.c_str());
 
     switch (match->second)
     {
       case SAFStatisticMeasure::MThroughput:
+      {
         return boost::shared_ptr<Mratio>(new Mratio(faces));
-        break;
+      }
       case SAFStatisticMeasure::MDelay:
       {
         //check for known attributes
@@ -69,6 +70,25 @@ boost::shared_ptr<SAFStatisticMeasure> SAFMeasureFactory::getMeasure(std::string
         }
         return boost::shared_ptr<Mratio>(new MDelay(faces, default_delay));
       }
+    case SAFStatisticMeasure::MHop:
+    {
+      //check for known attributes
+      int max_hops = 10; //ms
+
+      MeasureAttributeMap::iterator it = amap.find (match->first);
+
+      if(it != amap.end ())
+      {
+        for(std::map<std::string, std::string>::iterator k = it->second.begin(); k != it->second.end(); k++)
+        {
+          if(k->first.compare("MaxHops") == 0)
+          {
+            max_hops = boost::lexical_cast<int>(k->second);
+          }
+        }
+      }
+      return boost::shared_ptr<Mratio>(new MHop(faces, max_hops));
+    }
       default:
         return boost::shared_ptr<Mratio>(new Mratio(faces));
     }
